27e9a47717c51cedd92330f8642fd1205642351a
Roberto Valle <r.vallef@alumnos.upm.es>
2017-05-30 12:55:00 +0200

    Evaluate result and comment viewer

--- test/face_headpose_cnn_test.cpp
+++ test/face_headpose_cnn_test.cpp
@@ -9,6 +9,7 @@
 
 // ----------------------- INCLUDES --------------------------------------------
 #include <vector>
+#include <fstream>
 #include <opencv2/opencv.hpp>
 #include <boost/shared_ptr.hpp>
 
@@ -36,15 +37,15 @@ main
   // Read sample annotations
   upm::FaceAnnotation ann;
   ann.filename = "test/image68232.jpg";
-  ann.bbox.pos = cv::Rect2f(103, 78, 114, 114);
-  ann.headpose = cv::Point3f(-18.5096430850602, -3.10501745129284, -17.5719175410085);
+  ann.bbox.pos = cv::Rect2f(103.0f, 78.0f, 114.0f, 114.0f);
+  ann.headpose = cv::Point3f(-18.509f, -3.105f, -17.571f);
   cv::Mat frame = cv::imread(ann.filename, cv::IMREAD_COLOR);
   if (frame.empty())
     return EXIT_FAILURE;
 
   // Set face detected position
   std::vector<upm::FaceAnnotation> faces(1);
-  faces[0].bbox.pos =  ann.bbox.pos;
+  faces[0].bbox.pos = ann.bbox.pos;
 
   /// Load face components
   boost::shared_ptr<upm::FaceComposite> composite(new upm::FaceComposite());
@@ -57,12 +58,16 @@ main
 
   // Process frame
   double ticks = processFrame(frame, composite, faces, ann);
-  UPM_PRINT("FPS = " << static_cast<double>(cv::getTickFrequency())/ticks);
+  UPM_PRINT("FPS = " << cv::getTickFrequency()/ticks);
+
+  // Evaluate results
+  boost::shared_ptr<std::ostream> output(&std::cout, [](std::ostream*){});
+  composite->evaluate(output, faces, ann);
 
   // Draw results
-  boost::shared_ptr<upm::Viewer> viewer(new upm::Viewer);
-  viewer->init(0, 0, "face_headpose_cnn_test");
-  showResults(viewer, ticks, 1000, frame, composite, faces, ann);
+//  boost::shared_ptr<upm::Viewer> viewer(new upm::Viewer);
+//  viewer->init(0, 0, "face_headpose_cnn_test");
+//  showResults(viewer, ticks, 0, frame, composite, faces, ann);
 
   UPM_PRINT("End of face_headpose_cnn_test");
   return EXIT_SUCCESS;
